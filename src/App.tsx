87b1d4f7a361632274e11274e01036f39d338896
Gabriela Paredes <gabiprds.93@gmail.com>
2024-07-13 12:18:42 -0500

    Search countries by name and show it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useCountries } from "./hooks/countries"
+import { ChangeEventHandler, useState } from "react"
+
+import { useCountriesByName } from "./hooks/countries"
 
 function App() {
-  const { data } = useCountries()
+  const [countryToSearch, setCountryToSearch] = useState("")
+  const { data: countriesSearched } = useCountriesByName(countryToSearch)
+
+  const handleSearch: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    const { value } = target
+    const formatedValue = value.charAt(0).toUpperCase() + value.slice(1)
+
+    setCountryToSearch(formatedValue)
+  }
 
   return (
     <main>
+      <input
+        onChange={handleSearch}
+        placeholder="Escribir nombre del país"
+        type="text"
+        value={countryToSearch}
+      />
+
       <ul>
-        {data?.countries.map((country) => {
+        {countriesSearched?.countries.map((country) => {
           return (
             <li key={country.code}>{`${country.emoji} ${country.name}`}</li>
           )
--- src/hooks/countries.ts
+++ src/hooks/countries.ts
@@ -2,26 +2,42 @@ import { gql, useQuery } from "@apollo/client"
 
 import { ICountriesQuery } from "../lib/definitions"
 
+const countryFragment = `
+  code
+  name
+  emoji
+  currency
+  continent {
+    code
+    name
+  }
+  languages {
+    code
+    name
+  }
+  capital
+`
+
 export const useCountries = () => {
   const query = gql`
     query {
       countries {
-        code
-        name
-        emoji
-        currency
-        continent {
-          code
-          name
-        }
-        languages {
-          code
-          name
-        }
-        capital
+        ${countryFragment}
       }
     }
   `
 
   return useQuery<ICountriesQuery>(query)
+}
+
+export const useCountriesByName = (name: string) => {
+  const query = gql`
+    query ($name: String) {
+      countries(filter: { name: { regex: $name } }) {
+        ${countryFragment}
+      }
+    }
+  `
+
+  return useQuery<ICountriesQuery>(query, { variables: { name } })
 }
