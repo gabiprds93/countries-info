a2ab1e00afdac030971b0ee8505db17302dd510f
Gabriela Paredes <gabiprds.93@gmail.com>
2024-07-15 09:33:28 -0500

    Add test to make sure the Country page have a correct text

---
+++ src/tests/Country.test.tsx
@@ -0,0 +1,19 @@
+import { test, expect } from "vitest"
+import { MemoryRouter } from "react-router-dom"
+import { MockedProvider } from "@apollo/client/testing"
+import { render, screen } from "@testing-library/react"
+
+import CountryPage from "../pages/Country"
+
+test("Renders Country page", () => {
+  render(
+    <MockedProvider addTypename={false}>
+      <MemoryRouter>
+        <CountryPage />
+      </MemoryRouter>
+    </MockedProvider>
+  )
+
+  const textElement = screen.getByText("Información del país")
+  expect(textElement).toBeTruthy()
+})
